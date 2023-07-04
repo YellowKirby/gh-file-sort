a801eac6e0dd1e63b4d1c9c6ff9a888034c72988
chase <1654019+YellowKirby@users.noreply.github.com>
2023-07-04 10:43:02 -0700

    use ID + key from Chrome Web Store

--- src/manifest.ts
+++ src/manifest.ts
@@ -7,6 +7,7 @@ export function getManifest(): chrome.runtime.ManifestV3 {
     description: pkg.description,
     name: "Github Diff Order",
     version: pkg.version,
+    key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlkwDFPVVEFjg97W3gBz4u4NdrxtVc2mtNcEoqLCfk06JIlnp2qjfQDRGVPS1nkpCoD08GMDnWQpjWh0f6VtWmp+Cmk9dJ7C+m1sQgrl6cBjo5QfAnfbUAjdEFYQQAKftkiN/zsWITUfML35nya3ki1+HMEeKy1ZEUhcD80qhzsvrLLy3+0Dabj1m7oPQyQ7+4bjG6GIz5kxlBLvkBl6JE0W7sLUQ/y6gwyFM9mf8uWfDw1D2YiunWTPtQhlHfG4SFwsHwiltqpE7mAFJyxQvzDw2RUfMMujrFgWP0TuilyB2aT+RORmVkHKBL6OOANre/XWet+xPvWkIdbrWcpBa2QIDAQAB",
     content_scripts: [
       {
         js: ["src/content-script/content-script.ts"],
@@ -25,5 +26,10 @@ export function getManifest(): chrome.runtime.ManifestV3 {
       default_popup: "src/popup/index.html",
     },
     permissions: ["storage"],
+    browser_specific_settings: {
+      gecko: {
+        id: "{eb9647c9-cf86-4052-b65b-c30551922e7b}",
+      },
+    },
   };
 }
